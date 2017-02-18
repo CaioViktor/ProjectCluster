e0f63aa608ae78599ce959a5706842a45ccb2e7b
Caio Viktor <caioviktor@hotmail.com>
2017-02-18 15:04:40 -0300

    alguns comentários

--- controlCluster.py
+++ controlCluster.py
@@ -6,6 +6,7 @@ import modelCliente as cli
 
 def toMatrix(idToCliente):
 	matriz = list()
+	#cria matriz(narray) contendo a quantidade de pedidos e a soma dos valores dos pedidos dos clientes
 	for clienteId in idToCliente:
 		cliente = idToCliente[clienteId]
 		linha = list()
@@ -20,8 +21,10 @@ def clusterizar(matriz,idToCliente,limiteClusters=10,limiar=0.01):
 	classes = {}
 	k_range = range(1,limiteClusters+1)
 
+	#clusteriza com k clusters para todo 0<k<=limiteClusters
 	kmeans = [KMeans(init='k-means++',n_clusters=k).fit(matriz) for k in k_range]
 
+	#recupera centroides para calcular o numero adequado de clusters
 	centroids = [x.cluster_centers_ for x in kmeans]
 
 	k_eclid = [scipy.spatial.distance.cdist(matriz,cent,'euclidean') for cent in centroids]
@@ -29,23 +32,23 @@ def clusterizar(matriz,idToCliente,limiteClusters=10,limiar=0.01):
 
 	somaQuadrados = [sum(d**2) for d in distancia]
 
-
+	#normaliza soma dos quadrados das distancias dos membros para os centroids
 	delta = (somaQuadrados - min(somaQuadrados)) / (max(somaQuadrados)-min(somaQuadrados))
-	# print(delta)
 
+	#escolhe o numero de clusters que atendem ao limiar solicitado
 	clusters = 0
 	for x in range(0,limiteClusters):
 		if(abs(delta[x] - delta[x-1]) < limiar):
 			break
 		clusters+=1
-
-
-	# print("clusters: "+str(clusters)+" ----- "+str(delta[clusters]))
 	if clusters >= limiteClusters:
 		clusters = limiteClusters - 1
+
+
 	classificados = kmeans[clusters].labels_
 	centroid = centroids[clusters]
 	cont = 0
+	#relaciona clientes com suas classes
 	for clienteId in idToCliente:
 		cliente = idToCliente[clienteId]
 		cliente.setClasse(classificados[cont])
--- server.py
+++ server.py
@@ -9,42 +9,54 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "static/input"
 
 @app.route('/')
-def index(mensagem=""):
-	if os.path.isfile("static/input/input.csv"):
-		return redirect(url_for('list'))
-	return render_template("index.html",mensagem=mensagem)
+def index():
+	mensagem = ""
+	if 'mensagem' in request.args:
+		mensagem = request.args['mensagem']
+	if os.path.isfile("static/input/input.csv"):#verifica se ja existe um arquivo upado para o servidor
+		return redirect(url_for('list'))#clusteriza arquivo ja existente
+	return render_template("index.html",mensagem=mensagem)#pagina para configuracao de nova culsterizacao
 
 @app.route('/new')
-def new():
+def new():#apaga arquivo ja existe no servidor e redireciona para a pagina de configuracao de nova culsterizacao
 	os.system("rm static/input/input.csv")
 	return redirect(url_for('index'))
 
 
 @app.route('/cluster',methods = ['POST'])
 def cluster():
+	#recupera valores de configuracao
 	clustersMax = request.form['clusters']
 	limiar = request.form['limiar']
-	if 'arquivo' not in request.files:
-		return redirect(url_for("index"),mensagem="E necessario enviar um arquivo")
+
+	if 'arquivo' not in request.files:#verifica se o arquivo foi enviado
+		return redirect(url_for("index",mensagem="E necessario enviar um arquivo"))
 	file = request.files['arquivo']
 	if file.filename == "":
-		return redirect(url_for("index"),mensagem="Arquivo invalido")
+		return redirect(url_for("index",mensagem="Arquivo invalido"))
+
+	#remove possivel arquivo que ja possa existir no servidor
 	os.system("rm static/input/input.csv")
+	
+	#salva novo arquivo
 	nomeArquivo = "input.csv"
 	file.save(os.path.join(app.config['UPLOAD_FOLDER'],nomeArquivo))
 	return redirect(url_for('list',clustersMax = clustersMax,limiar = limiar))
 
 @app.route('/list')
 def list():
+	#recupera valores de configuracao
 	clustersMax = int(request.args['clustersMax'])
 	limiar = float(request.args['limiar'])
-	classificacao = cc.classificar(clustersMax,limiar)
-	classes = classificacao['classes']
-	# for classeID in classes:
-	# 	for cliente in classes[classeID]:
-	# 		print(cliente) 
-	clientes = classificacao['clientes']
-	return render_template("lista.html",classes = classes,clientes = clientes)
 
+	#classifica
+	try:
+		classificacao = cc.classificar(clustersMax,limiar)
+		classes = classificacao['classes']
+		clientes = classificacao['clientes']
+		return render_template("lista.html",classes = classes,clientes = clientes)
+	except Exception as erro:
+		os.system("rm static/input/input.csv")
+		return redirect(url_for('index',mensagem="Arquivo invalido"))
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
